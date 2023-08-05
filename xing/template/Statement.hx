2da28669a763c318f8c50dfb81573aac4ee0504b
nevergarden <nevergarden.fa@gmail.com>
2023-08-05 11:54:54 +0100

    add: statement bodies

--- xing/template/Statement.hx
+++ xing/template/Statement.hx
@@ -1,31 +1,30 @@
 package xing.template;
 
 interface Statement {
+	public var kind(get, never):StatementKind;
 	public function toString():String;
 }
 
 class ExpressionStatement implements Statement {
-	private final expression:Expression;
+	public final expression:Expression;
+	public var kind(get, never):StatementKind;
 
 	public function new(expression:Expression) {
 		this.expression = expression;
 	}
 
 	public function toString():String {
-		return "Expression: " + this.expression.toString();
+		return "expression: " + this.expression.toString();
 	}
-}
 
-class VariableStatement implements Statement {
-	public function new(name:Token, initializer:Expression) {}
-
-	public function toString():String {
-		return "Variable";
+	function get_kind():StatementKind {
+		return SExpression;
 	}
 }
 
 class BlockStatement implements Statement {
-	private final statements:Array<Statement>;
+	public final statements:Array<Statement>;
+	public var kind(get, never):StatementKind;
 
 	public function new(statements:Array<Statement>) {
 		this.statements = statements;
@@ -43,40 +42,84 @@ class BlockStatement implements Statement {
 		s += "}";
 		return s;
 	}
+
+	function get_kind():StatementKind {
+		return SBlock;
+	}
 }
 
 class IfStatement implements Statement {
-	public function new(condition:Expression, thenBlock:Statement, elseBlock:Statement) {}
+	public final condition:Expression;
+	public final thenBlock:Statement;
+	public final elseBlock:Statement;
+	public var kind(get, never):StatementKind;
+
+	public function new(condition:Expression, thenBlock:Statement, elseBlock:Statement) {
+		this.condition = condition;
+		this.thenBlock = thenBlock;
+		this.elseBlock = elseBlock;
+	}
 
 	public function toString():String {
-		return "If";
+		return 'if cond: ${condition} then: ${thenBlock} else: ${elseBlock}';
+	}
+
+	function get_kind():StatementKind {
+		return SIf;
 	}
 }
 
 class WhileStatement implements Statement {
-	public function new(condition:Expression, body:Statement) {}
+	public final condition:Expression;
+	public final body:Statement;
+	public var kind(get, never):StatementKind;
+
+	public function new(condition:Expression, body:Statement) {
+		this.condition = condition;
+		this.body = body;
+	}
 
 	public function toString():String {
-		return "While";
+		return 'while: cond: ${condition} body: ${body}';
+	}
+
+	function get_kind():StatementKind {
+		return SWhile;
 	}
 }
 
 class ForStatement implements Statement {
-	public function new(condition:Expression, body:Statement) {}
+	public final condition:Expression;
+	public final body:Statement;
+	public var kind(get, never):StatementKind;
+
+	public function new(condition:Expression, body:Statement) {
+		this.condition = condition;
+		this.body = body;
+	}
 
 	public function toString():String {
-		return "For";
+		return 'for: cond: ${condition} body: ${body}';
+	}
+
+	function get_kind():StatementKind {
+		return SFor;
 	}
 }
 
 class DocStatement implements Statement {
 	public final token:Token;
+	public var kind(get, never):StatementKind;
 
 	public function new(token:Token) {
 		this.token = token;
 	}
 
 	public function toString():String {
-		return "Doc: " + token.literal;
+		return "doc: " + token.literal;
+	}
+
+	function get_kind():StatementKind {
+		return SDoc;
 	}
 }
